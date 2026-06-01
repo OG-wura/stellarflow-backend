78b570ac2ffa37a190379e44ca658dc8d9031cf5
AYOMI <okelanaayomide@gmail.com>
2026-06-01 14:25:59 +0000

    feat(analytics): implement exponential moving average functions and classes

---
+++ src/analytics/__init__.py
@@ -0,0 +1,9 @@
+from .ema import RollingEMA, update_ema, ema_sequence, smoothing_factor, progressive_smoothing_factor
+
+__all__ = [
+    "RollingEMA",
+    "update_ema",
+    "ema_sequence",
+    "smoothing_factor",
+    "progressive_smoothing_factor",
+]
